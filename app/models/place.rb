432c13b4329fbd12a15bf975a8546b4710159b70
Abiy-Kaltiso <abiyk7@gmail.com>
2016-01-17 13:30:09 +0000

    added map stuff

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,8 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
-	validates :address, :presence => true, :length => {:minimum => 3}
-	validates :description, :presence => true
-
 	geocoded_by :address
 	after_validation :geocode
+	
+	validates :name, :presence => true
+	validates :description, :presence => true
 end
